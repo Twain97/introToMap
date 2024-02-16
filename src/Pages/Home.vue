a8b8f6db27d9d40f86d1e2e277124f3759583abf
Akinpelumi Taiwo <sammytwain97@gmail.com>
2024-02-16 23:10:08 +0100

    Added Tailwind css

---
+++ src/Pages/Home.vue
@@ -0,0 +1,14 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div class="bg-red-600 text-center py-44">Home Page</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
